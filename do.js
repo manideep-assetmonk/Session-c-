9309cde83b1b1cc59a0ed2eaaa7d682ca8fdb47c
manideep-assetmonk <103191212+manideep-assetmonk@users.noreply.github.com>
2022-04-12 15:01:02 +0530

    promise-it-wont-hurt

--- do.js
+++ do.js
@@ -1,4 +1,8 @@
 var abc= require('q-io/http');
+/**
+ * read() enables u to read the contain of a file object in to memory.
+ */
+
 abc.read("http://localhost:7000")
 
   .then(function (id){
@@ -7,6 +11,10 @@ abc.read("http://localhost:7000")
   })
   .then(function (json){
     console.log(JSON.parse(json));
+       /**
+   * The console. error() method outputs an error message to the Web console.
+     * 
+     */
   })
   .then(null,console.error)
   .done()
