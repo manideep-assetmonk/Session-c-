f38e90c438cfa71a48c877158d1c3a0458a1e519
manideep-assetmonk <103191212+manideep-assetmonk@users.noreply.github.com>
2022-04-12 15:01:57 +0530

    promise-it-wont-hurt

--- fetch.js
+++ fetch.js
@@ -4,5 +4,9 @@ abc.read("http://localhost:1337")
   .then(function (json){
       console.log(JSON.parse(json));
   })
+     /**
+   * The console. error() method outputs an error message to the Web console.
+     * 
+     */
   .then(null,console.error)
   .done()
